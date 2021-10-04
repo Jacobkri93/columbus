001462b9122ef3fffd7525a73cf27428352f265a
Jacobkri93 <70961507+Jacobkri93@users.noreply.github.com>
2021-10-04 10:03:08 +0200

    Initial commit

---
+++ src/main/java/dk/kea/columbus/ColumbusApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.columbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumbusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColumbusApplication.class, args);
+    }
+
+}
