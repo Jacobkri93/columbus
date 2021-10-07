fb84e3111854f7dc3fe5615d5054a179e98b3663
Mick3460 <mich8g56@stud.kea.dk>
2021-10-07 11:07:56 +0200

    Ændringer i properties, inserts og homecontroller metoder indsat

--- src/main/java/dk/kea/columbus/Model/Tile.java
+++ src/main/java/dk/kea/columbus/Model/Tile.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Tile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id_tile;
 
     @Column(nullable = true) // idk
     private int x_pos;
@@ -36,22 +36,6 @@ public class Tile {
     @Column(nullable = false) // idk
     private int tile_type;
 
-    @Column(nullable = true) // idk
-    private String tile_type_name;
-
-    @Column(nullable = false)
-    private int movement_points;
-
-    @Column(nullable = false)
-    private int id_civilisation;
-
-    @Column(nullable = true)
-    private String civilisation_name;
-
-    @Column(nullable = true)
-    private String attitude;
-
-
     public Tile() {
 
     }
@@ -66,15 +50,11 @@ public class Tile {
         this.tile_at_point_five = tile_at_point_five;
         this.tile_at_point_six = tile_at_point_six;
         this.tile_type = tile_type;
-        this.tile_type_name = tile_type_name;
-        this.movement_points = movement_points;
-        this.id_civilisation = id_civilisation;
-        this.civilisation_name = civilisation_name;
-        this.attitude = attitude;
+
     }
 
-    public Tile(Long id, int x_pos, int y_pos, boolean tile_at_point_one, boolean tile_at_point_two, boolean tile_at_point_three, boolean tile_at_point_four, boolean tile_at_point_five, boolean tile_at_point_six, int tile_type, String tile_type_name, int movement_points, int id_civilisation, String civilisation_name, String attitude) {
-        this.id = id;
+    public Tile(Long id_tile, int x_pos, int y_pos, boolean tile_at_point_one, boolean tile_at_point_two, boolean tile_at_point_three, boolean tile_at_point_four, boolean tile_at_point_five, boolean tile_at_point_six, int tile_type, String tile_type_name, int movement_points, int id_civilisation, String civilisation_name, String attitude) {
+        this.id_tile = id_tile;
         this.x_pos = x_pos;
         this.y_pos = y_pos;
         this.tile_at_point_one = tile_at_point_one;
@@ -84,19 +64,15 @@ public class Tile {
         this.tile_at_point_five = tile_at_point_five;
         this.tile_at_point_six = tile_at_point_six;
         this.tile_type = tile_type;
-        this.tile_type_name = tile_type_name;
-        this.movement_points = movement_points;
-        this.id_civilisation = id_civilisation;
-        this.civilisation_name = civilisation_name;
-        this.attitude = attitude;
+
     }
 
-    public Long getId() {
-        return id;
+    public Long getId_tile() {
+        return id_tile;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId_tile(Long id) {
+        this.id_tile = id;
     }
 
     public int getX_pos() {
@@ -171,50 +147,11 @@ public class Tile {
         this.tile_type = tile_type;
     }
 
-    public String getTile_type_name() {
-        return tile_type_name;
-    }
-
-    public void setTile_type_name(String tile_type_name) {
-        this.tile_type_name = tile_type_name;
-    }
-
-    public int getMovement_points() {
-        return movement_points;
-    }
-
-    public void setMovement_points(int movement_points) {
-        this.movement_points = movement_points;
-    }
-
-    public int getId_civilisation() {
-        return id_civilisation;
-    }
-
-    public void setId_civilisation(int id_civilisation) {
-        this.id_civilisation = id_civilisation;
-    }
-
-    public String getCivilisation_name() {
-        return civilisation_name;
-    }
-
-    public void setCivilisation_name(String civilisation_name) {
-        this.civilisation_name = civilisation_name;
-    }
-
-    public String getAttitude() {
-        return attitude;
-    }
-
-    public void setAttitude(String attitude) {
-        this.attitude = attitude;
-    }
 
     @Override
     public String toString() {
         return "Tile{" +
-                "id=" + id +
+                "id=" + id_tile +
                 ", x_pos=" + x_pos +
                 ", y_pos=" + y_pos +
                 ", tile_at_point_one=" + tile_at_point_one +
@@ -224,11 +161,6 @@ public class Tile {
                 ", tile_at_point_five=" + tile_at_point_five +
                 ", tile_at_point_six=" + tile_at_point_six +
                 ", tile_type=" + tile_type +
-                ", tile_type_name='" + tile_type_name + '\'' +
-                ", movement_points=" + movement_points +
-                ", id_civilisation=" + id_civilisation +
-                ", civilisation_name='" + civilisation_name + '\'' +
-                ", attitude='" + attitude + '\'' +
                 '}';
     }
 }
